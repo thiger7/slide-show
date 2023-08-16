9e08195aafe049eefee85a85cdebfeb07ead0c1a
adf0423 <adf0423@gmail.com>
2023-08-17 00:13:10 +0900

    refactor

--- sample.js
+++ sample.js
@@ -10,18 +10,17 @@ window.onload = () => {
    *===============================================================*/
 
   // 画像のリストの定義
-  let photoList = [
-    { src: 'img/spring.jpeg', title: '春の桜' },
-    { src: 'img/summer.jpeg', title: '夏のひまわり' },
-    { src: 'img/autumn.jpeg', title: '秋の紅葉' },
-    { src: 'img/winter.jpeg', title: '冬の山' },
+  const photoList = [
+    { id: 1, src: 'img/spring.jpeg', title: '春の桜' },
+    { id: 2, src: 'img/summer.jpeg', title: '夏のひまわり' },
+    { id: 3, src: 'img/autumn.jpeg', title: '秋の紅葉' },
+    { id: 4, src: 'img/winter.jpeg', title: '冬の山' },
   ];
-  let photoLength = photoList.length;
 
   //　要素の取得
-  let photo = document.getElementById('photo');
-  let nextBtn = document.getElementById('nextBtn');
-  let title = document.getElementById('title');
+  const photo = document.getElementById('photo');
+  const nextBtn = document.getElementById('nextBtn');
+  const title = document.getElementById('title');
 
   // 現在のインデックスを保存するための変数
   let currentIndex = 0;
@@ -32,17 +31,18 @@ window.onload = () => {
 
   // 指定の画像に表示を切り替える関数
   const showPhoto = (index) => {
+
     // すべての画像を非表示
-    for (let i = 0; i < photoLength; i++) {
-      photoList[i].elem.style.display = 'none';
-    }
+    photoList.map(item => {
+      // 各要素の表示を非表示に設定
+      item.elem.style.display = 'none';
+    });
 
     // 表示する対象の要素を取得
-    let targetPhoto = photoList[index];
+    const targetPhoto = photoList[index];
 
     // タイトルの表示
-    let viewNumber = index + 1;
-    title.innerHTML = `[${viewNumber}] ${targetPhoto.title}`;
+    title.innerHTML = `[${targetPhoto.id}] ${targetPhoto.title}`;
 
     // 画像の表示
     targetPhoto.elem.style.display = 'inline';
@@ -55,10 +55,7 @@ window.onload = () => {
   // nextボタンを押したときの処理
   nextBtn.onclick = () => {
     // 表示する画像のインデックスを計算
-    currentIndex++;
-    if (currentIndex === photoLength) {
-      currentIndex = 0;
-    }
+    currentIndex = (currentIndex + 1) % photoList.length;
 
     // 画像の切り替え
     showPhoto(currentIndex);
@@ -69,12 +66,9 @@ window.onload = () => {
    *===============================================================*/
 
   // img要素をHTMLに追加
-  let item, img;
-  for (let i = 0; i < photoLength; i++) {
-    item = photoList[i];
-
+  photoList.map(item => {
     // img要素の作成
-    img = document.createElement('img');
+    const img = document.createElement('img');
 
     // 作成したimg要素に属性を設定
     img.src = item.src;
@@ -85,7 +79,7 @@ window.onload = () => {
 
     // img要素をキャッシュする
     item.elem = img;
-  }
+  });
 
   // 初期表示のためにshowPhoto関数を実行する
   showPhoto(currentIndex);
