030b95435879f1f68809172917a7997b1726aa64
adf0423 <adf0423@gmail.com>
2023-08-16 22:48:52 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  //　要素の取得
+  let photo = document.getElementById('photo');
+  let nextBtn = document.getElementById('nextBtn');
+  let title = document.getElementById('title');
+
+  console.log(photo);
+  console.log(nextBtn);
+  console.log(title);
+}
