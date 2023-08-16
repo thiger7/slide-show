15721d0819397fd265f0d2bc480509fa30cc89e5
adf0423 <adf0423@gmail.com>
2023-08-16 22:52:33 +0900

    画像のリストの定義

--- sample.js
+++ sample.js
@@ -1,10 +1,15 @@
 window.onload = () => {
+  // 画像のリストの定義
+  let photoList = [
+    { src: 'img/spring.jpeg', title: '春の桜' },
+    { src: 'img/summer.jpeg', title: '夏のひまわり' },
+    { src: 'img/autumn.jpeg', title: '秋の紅葉' },
+    { src: 'img/winter.jpeg', title: '冬の山' },
+  ];
+  let photoLength = photoList.length;
+
   //　要素の取得
   let photo = document.getElementById('photo');
   let nextBtn = document.getElementById('nextBtn');
   let title = document.getElementById('title');
-
-  console.log(photo);
-  console.log(nextBtn);
-  console.log(title);
 }
